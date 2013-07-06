52c3c033b40e5dc51196f56d171d7fe7b75c9e96
xiaohan <xiaohan2012@gmail.com>
2013-07-06 11:14:57 +0800

    left side tab added

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+    $('#left-tabs a').click(function (e) {
+	e.preventDefault();
+	$(this).tab('show');
+	//badge-warning
+    }).on('shown', function (e) {
+	var $this = $(e.target); // activated tab
+	$this.children(".badge").addClass("badge-warning");
+
+	var $prev = $(e.relatedTarget);
+	$prev.children(".badge").removeClass("badge-warning");
+    });
+})
